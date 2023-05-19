e7a0d0dc7a704068a9db17066817bf35057708cf
TH876 <tayhiggins14@gmail.com>
2023-05-19 06:48:44 -0400

    styled components

--- src/pages/MoreDetails.js
+++ src/pages/MoreDetails.js
@@ -9,8 +9,8 @@ function MoreDetails() {
     return (
         <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
             <div className="flow-root" key={employee.id}>
-                <ul role="list" className="bg-white p-2 border-b-2 border-gray-100">
-                    <li className="py-3 sm:py-4">
+                <ul role="list">
+                    <li className="py-3 sm:py-4 bg-white p-2 border-b-2 border-gray-100">
                         <div className="flex items-center space-x-4">
                             <div className="flex-shrink-0">
                                 <img className="w-20 h-auto rounded-full" src={employee.image} alt={employee.name}/>
@@ -21,22 +21,29 @@ function MoreDetails() {
                                 <p className="text-sm text-gray-500 truncate dark:text-gray-400">{employee.position}</p>
                             </div>
                         </div>
-                        
+                    </li>
+                    <li className="py-3 sm:py-4 bg-white p-2 border-b-2 border-gray-100">
                         <div className="flex-1 min-w-0">
                             <p className="text-sm font-medium text-gray-900 truncate dark:text-white">Office</p>
                             <p className="text-sm text-gray-500 truncate dark:text-gray-400">{employee.officenumber}</p>
                         </div>
+                    </li>
 
+                    <li className="py-3 sm:py-4 bg-white p-2 border-b-2 border-gray-100">
                         <div className="flex-1 min-w-0">
                             <p className="text-sm font-medium text-gray-900 truncate dark:text-white">Mobile</p>
                             <p className="text-sm text-gray-500 truncate dark:text-gray-400">{employee.mobilenumber}</p>
                         </div>
+                    </li>
 
+                    <li className="py-3 sm:py-4 bg-white p-2 border-b-2 border-gray-100">
                         <div className="flex-1 min-w-0">
                             <p className="text-sm font-medium text-gray-900 truncate dark:text-white">SMS</p>
                             <p className="text-sm text-gray-500 truncate dark:text-gray-400">{employee.smsnumber}</p>
                         </div>
+                    </li>
 
+                    <li className="py-3 sm:py-4 bg-white p-2 border-b-2 border-gray-100">
                         <div className="flex-1 min-w-0">
                             <p className="text-sm font-medium text-gray-900 truncate dark:text-white">Email</p>
                             <p className="text-sm text-gray-500 truncate dark:text-gray-400">{employee.email}</p>
